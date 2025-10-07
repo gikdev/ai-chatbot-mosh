d9482c3a830dcaeeb3684a76a7b847c6edc4fc47
gikdev <m.m.bhrm85@gmail.com>
2025-10-07 15:12:16 +0330

    Set husky up

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -7,12 +7,14 @@ export default function App() {
   useEffect(() => {
     fetch("/api/hello")
       .then(res => res.json())
+
       .then(data => setMsg(data.message))
   }, [])
 
   return (
     <div className="p-4">
       <p className="font-bold">{msg}</p>
+
       <Button>Click me</Button>
     </div>
   )
